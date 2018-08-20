8830fff65f662a90aeeac3bf6de3738d75fa6ea0
andrefleifel <19fleifela63@stu.smuhsd.org>
2018-08-20 09:45:49 -0700

    First project to commit to Github

---
+++ First Test/src/Andre_Class.java
@@ -0,0 +1,17 @@
+
+public class Andre_Class {
+
+	/**
+	 * Write a description of class Hello_world here.
+	 * 
+	 * @author (Andre Fleifel) 
+	 * @version (1.0)
+	 */
+	
+	    public static void main(String[] args)
+	    
+	        System.out.println("Hello World! From <Andre>"); //Display the string
+	    }
+	}
+
+}
