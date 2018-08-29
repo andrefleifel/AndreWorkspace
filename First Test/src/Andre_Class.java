8ce81592b8d1ffc4f1dd37fda572cc757c6ec4c1
andrefleifel <19fleifela63@stu.smuhsd.org>
2018-08-29 10:30:04 -0700

    test

--- First Test/src/Andre_Class.java
+++ First Test/src/Andre_Class.java
@@ -1,17 +1,16 @@
 
-public class Andre_Class {
 
 	/**
-	 * Write a description of class Hello_world here.
-	 * 
+	 * Print out Hello Word! From Andre 
+	 * @date (8-26-18)
 	 * @author (Andre Fleifel) 
 	 * @version (1.0)
 	 */
+public class Andre_Class 
+{
 	
-	    public static void main(String[] args)
-	    
-	        System.out.println("Hello World! From <Andre>"); //Display the string
-	    }
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World! From Andre"); //Display the string
 	}
-
 }
