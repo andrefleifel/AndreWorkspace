4ab475159cd9ee5684e1e2b74d815b288f4a7d3a
andrefleifel <19fleifela63@stu.smuhsd.org>
2018-08-31 09:51:13 -0700

    Pikachu

---
+++ Pikachu/src/pikachuu.java
@@ -0,0 +1,16 @@
+
+/**
+ * Print out a pikachu
+ * @date (8-30-18)
+ * @author (Andre Fleifel) 
+ * @version (1.0)
+ */
+public class pikachuu 
+{
+
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t(\\__/)");
+		System.out.println("\t\t(o^.^)");
+	}
+}
