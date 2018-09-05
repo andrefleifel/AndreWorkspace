709b0fcaf45c7515c9c2fa942871957b5dc00a70
andrefleifel <19fleifela63@stu.smuhsd.org>
2018-09-05 09:35:56 -0700

    update pikachu

--- Pikachu/src/pikachuu.java
+++ Pikachu/src/pikachuu.java
@@ -12,5 +12,7 @@ public class pikachuu
 		System.out.println("Pikachu welcomes you to the world of Pokemon!");
 		System.out.println("\t\t(\\__/)");
 		System.out.println("\t\t(o^.^)");
+		System.out.println("\t      z(_(\")(\")");
 	}
 }
+
